30654908d167627b6c51b0c6756bae538e3dcd2f
karthickbsk08 <karthickbsk08@gmail.com>
2023-11-01 12:15:23 +0530

    Code updated

---
+++ newfile2.go
@@ -0,0 +1,14 @@
+package main
+
+import "log"
+
+func NewMEthod2() {
+	log.Println("Im the New method1")
+	log.Println("Im the New method2 --------wq")
+	log.Println("Im the New method2 -------My commit 2")
+
+	log.Println("New file changes")
+	log.Println("New file changes")
+	log.Println("New file changes")
+	log.Println("New file changes")
+}
